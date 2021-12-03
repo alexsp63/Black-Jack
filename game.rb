40249cc9a0929c367faaf147b92e7ea8ceb56318
Sofya <sofya.popowa@yandex.ru>
2021-12-04 00:51:58 +0300

    made consts private

--- game.rb
+++ game.rb
@@ -3,8 +3,6 @@
 require_relative 'imports'
 
 class Game
-  VALUES = %w[2 3 4 5 6 7 8 9 J Q K A].freeze
-  SUITS = ["\u2660", "\u2665", "\u2666", "\u2663"].freeze
 
   def initialize(user, dealer)
     @interface = Interface.new
@@ -39,6 +37,9 @@ class Game
   end
 
   private
+
+  VALUES = %w[2 3 4 5 6 7 8 9 J Q K A].freeze
+  SUITS = ["\u2660", "\u2665", "\u2666", "\u2663"].freeze
 
   def users_move
     case @interface.user_input(@user.cards.size == 2).to_i
